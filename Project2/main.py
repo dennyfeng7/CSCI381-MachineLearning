65db8c91b5231968d50bf446f24191f72192dc01
dennyfeng7 <denny.feng39@qmail.cuny.edu>
2020-10-16 14:20:01 -0400

    test update

--- Project2/main.py
+++ Project2/main.py
@@ -1,6 +1,12 @@
-def main():
-    print('test')
+import sklearn
+from sklearn import datasets
+import pandas as pd
 
+
+def main():
+    data = datasets.load_breast_cancer()
+    print(data.keys())
+    print(data.DESCR)
 
 if __name__ == '__main__':
     main()
