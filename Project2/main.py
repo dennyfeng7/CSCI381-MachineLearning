476adae40c0ca700370ccab9046814f605bb627a
dennyfeng7 <denny.feng39@qmail.cuny.edu>
2020-11-07 13:58:06 -0500

    Added pocket (perceptron learning) algorithm, temp PLA test.py file.

--- Project2/main.py
+++ Project2/main.py
@@ -9,3 +9,148 @@ from sklearn.model_selection import train_test_split
 # Load the data set
 data = sns.load_dataset("iris")
 data.head()
+
+# Prepare the training set
+# Independent set "X"
+# X = feature values, get all columns except the last column
+X = data.iloc[:, :-1] # gets all the rows, and then all columns except the last column
+
+#y = target values, we only want the last column of the data frame
+y = data.iloc[:, -1] # gets all rows, and gets only the last column
+
+# Plot the data. Plot the relation of each feature with each species
+
+plt.xlabel('Features')
+plt.ylabel('Species')
+
+# Now let's give it the data
+pltX = data.loc[:, 'sepal_length']
+pltY = data.loc[:, 'species']
+plt.scatter(pltX, pltY, color='blue', Label='sepal_length')
+
+pltX = data.loc[:, 'sepal_width']
+pltY = data.loc[:, 'species']
+plt.scatter(pltX, pltY, color='green', Label='sepal_width')
+
+pltX = data.loc[:, 'petal_length']
+pltY = data.loc[:, 'species']
+plt.scatter(pltX, pltY, color='red', Label='petal_length')
+
+pltX = data.loc[:, 'petal_width']
+pltY = data.loc[:, 'species']
+plt.scatter(pltX, pltY, color='black', Label='petal_width')
+
+plt.legend(loc = 4, prop={'size':8})
+plt.show()
+
+# Split the data into 80% training and 20% testing
+x_train, x_test, y_train, y_test = train_test_split(X, y, test_size = 0.2) # train_test_split('independent dataset', 'dependent dataset')
+
+# Train the model. "fit" means to train
+model = LogisticRegression()
+model.fit(x_train, y_train) # Training the model
+
+# Test the model
+predictions = model.predict(x_test)
+# print(predictions)
+
+# print(y_test)
+
+# Check the precision, recall, f1-score
+print(classification_report(y_test, predictions))
+
+print(accuracy_score(y_test, predictions))
+
+print(__doc__)
+
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn import datasets
+from sklearn.linear_model import SGDClassifier
+
+# import data
+iris = datasets.load_iris()
+
+X = iris.data[:, :2]
+y = iris.target
+colors = "bry"
+
+# shuffle
+idx = np.arange(X.shape[0])
+np.random.seed(13)
+np.random.shuffle(idx)
+X = X[idx]
+y = y[idx]
+
+# standardize
+mean = X.mean(axis=0)
+std = X.std(axis=0)
+X = (X - mean) / std
+
+h = .02  # step size in the mesh
+
+clf = SGDClassifier(alpha=0.001, max_iter=100).fit(X, y)
+
+# create a mesh to plot in
+x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+xx, yy = np.meshgrid(np.arange(x_min, x_max, h),
+                     np.arange(y_min, y_max, h))
+
+# Plot the decision boundary. For that, we will assign a color to each
+# point in the mesh [x_min, x_max]x[y_min, y_max].
+Z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
+# Put the result into a color plot
+Z = Z.reshape(xx.shape)
+cs = plt.contourf(xx, yy, Z, cmap=plt.cm.Paired)
+plt.axis('tight')
+
+# Plot also the training points
+for i, color in zip(clf.classes_, colors):
+    idx = np.where(y == i)
+    plt.scatter(X[idx, 0], X[idx, 1], c=color, label=iris.target_names[i],
+                cmap=plt.cm.Paired, edgecolor='black', s=20)
+plt.title("Multi-Class Classification of 2 Breast Cancer Classes")
+plt.axis('tight')
+
+# Plot the three one-against-all classifiers
+xmin, xmax = plt.xlim()
+ymin, ymax = plt.ylim()
+coef = clf.coef_
+intercept = clf.intercept_
+
+
+def plot_hyperplane(c, color):
+    def line(x0):
+        return (-(x0 * coef[c, 0]) - intercept[c]) / coef[c, 1]
+
+    plt.plot([xmin, xmax], [line(xmin), line(xmax)],
+             ls="--", color=color)
+
+
+for i, color in zip(clf.classes_, colors):
+    plot_hyperplane(i, color)
+plt.legend()
+plt.show()
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+%matplotlib inline
+
+from sklearn.datasets import load_breast_cancer
+cancer = load_breast_cancer()
+
+df_cancer = pd.DataFrame(np.c_[cancer['data'], cancer['target']], columns = np.append(cancer['feature_names'], ['target']))
+
+df_cancer.head()
+
+sns.pairplot(df_cancer, hue = 'target', vars = ['mean radius', 'mean texture', 'mean perimeter', 'mean area', 'mean smoothness'])
+
+X = df_cancer.drop(['target'], axis = 1)
+X.head()
+
+y = df_cancer['target']
+y.head()
--- Project2/side.py
+++ Project2/side.py
@@ -1,65 +1,28 @@
+# Import our libraries
+
+# Pandas and numpy for data wrangling
+import inline as inline
+import matplotlib
+import pandas as pd
+import numpy as np
+
+# Seaborn / matplotlib for visualization
 import seaborn as sns
-from sklearn.datasets import load_breast_cancer
-from sklearn.linear_model import LogisticRegression
+sns.set()
+import matplotlib.pyplot as plt
+# %matplotlib inline
+
+
+# Helper function to split our data
 from sklearn.model_selection import train_test_split
 
+# This is our Logit model
+from sklearn.linear_model import LogisticRegression
 
-def side():
-    load_breast_cancer
-    # split the data
-    x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=.2)
-    # train the model
-    model = LogisticRegression()
-    model.fit(x_train, y_train)
-    # test the model
-    predictions = model.predict(x_test)
-    print(predictions)
+# Helper fuctions to evaluate our model.
+from sklearn.metrics import accuracy_score, precision_score, recall_score, confusion_matrix
+from sklearn.datasets import load_breast_cancer
 
-    print(y_test)
+df=pd.read_csv('data.csv')
 
-    from sklearn.model_selection import train_test_split
-    import matplotlib.pyplot as plt
-    data = sns.load_dataset("iris")
-    data.head()
-    # prep the training set
-    # X = feature values , all of the columsn except the last column
-    X = data.iloc[:, :-1]
-
-    # Y = target values
-
-    y = data.iloc[:, -1]
-    plt.xlabel('Features')
-    plt.ylabel('Species')
-
-    pltX = data.loc[:, 'sepal_length']
-    pltY = data.loc[:, 'species']
-    plt.scatter(pltX, pltY, color='blue', Label='sepal_length')
-
-    pltX = data.loc[:, 'sepal_width']
-    pltY = data.loc[:, 'species']
-    plt.scatter(pltX, pltY, color='green', Label='sepal_width')
-
-    pltX = data.loc[:, 'petal_length']
-    pltY = data.loc[:, 'species']
-    plt.scatter(pltX, pltY, color='yellow', Label='petal_length')
-
-    pltX = data.loc[:, 'petal_width']
-    pltY = data.loc[:, 'species']
-    plt.scatter(pltX, pltY, color='orange', Label='petal_width')
-
-    plt.legend(loc=4, prop={'size': 8})
-    plt.show()
-    # split the data
-    x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=.2)
-    # train the model
-    model = LogisticRegression()
-    model.fit(x_train, y_train)
-    # test the model
-    predictions = model.predict(x_test)
-    print(predictions)
-
-    print(y_test)
-
-
-if __name__ == '__main__':
-    side()
+df.head()
